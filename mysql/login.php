83e08e980df6895b54bd5668452f4b0db1aabcf4
Ryan Dulac <analyticrye@gmail.com>
2018-05-14 02:17:23 -0400

    more stuff

---
+++ mysql/login.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+</head>
+<body>
+
+	<div class="container">
+		<div class="col-xs-6">
+			<form action="login.php" method="post">
+				<div class="form-group">
+					<label for="username">Username</label>
+					<input type="text" name="username" class="form-control">
+				</div>
+
+				<div class="form-group">
+					<label for="password">Password</label>
+					<input type="password" name="password" class="form-control">
+				</div>
+
+				<input class="btn btn-primary" type="submit" name="submit" value="Submit">
+
+			</form>
+			
+
+		</div>
+		
+
+
+
+
+	</div>
+	
+</body>
+</html>
