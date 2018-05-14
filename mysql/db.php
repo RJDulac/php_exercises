208e21185f4ae76f27ff1ceb2d79f2f6b679f34f
Ryan Dulac <analyticrye@gmail.com>
2018-05-14 15:40:59 -0400

    more exercises added

---
+++ mysql/db.php
@@ -0,0 +1,12 @@
+<?php
+
+	$connection = mysqli_connect('localhost', 'root', '', 'loginapp');
+
+	if(!$connection) {
+		die("Database connection Failed");
+	}
+
+
+	//echo $x = ($connection) ? "We are connected" : die("Database connection failed!");
+
+	?>
---
+++ mysql/login_update.php
@@ -0,0 +1,57 @@
+<?php include "db.php";
+
+	 
+
+	$query = "SELECT * FROM users";
+	$result = mysqli_query($connection, $query);
+
+	if(!$result) {
+		die('Query FAILED!' . mysqli_error());
+	}
+
+
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+</head>
+<body>
+
+	<div class="container">
+		<div class="col-sm-6">
+
+
+		</div>
+			<form action="login_update.php" method="post">
+				<div class="form-group">
+					<label for="username">Username</label>
+					<input type="text" name="username" class="form-control">
+				</div>
+
+				<div class="form-group">
+					<label for="password">Password</label>
+					<input type="password" name="password" class="form-control">
+				</div>
+
+				<div class="form-group">
+					<select name="" id="">
+						<option value="">1</option>
+					</select>
+				</div>
+
+				<input class="btn btn-primary" type="submit" name="submit" value="UPDATE">
+
+			</form>
+
+	</div>
+	
+</body>
+</html>
